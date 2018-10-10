f75f53cbf5b47f53fde7cedb78ea69661ea1cd70
aferrero2707 <aferrero1975@gmail.com>
2018-10-10 12:56:00 +0200

    Added custom compiler flags to qmake

--- GMIC/build-osx.sh
+++ GMIC/build-osx.sh
@@ -21,7 +21,7 @@ if [ ! -e gmic-clone ]; then
 	make -C gmic-clone/src CImg.h gmic_stdlib.h || exit 1
 fi
 
-qmake CONFIG+=Release HOST=gimp GMIC_PATH=gmic-clone/src || exit 1
+qmake QMAKE_CFLAGS+="${CFLAGS}" QMAKE_CXXFLAGS+="${CXXFLAGS}" QMAKE_LFLAGS+="${LDFLAGS}" CONFIG+=Release HOST=gimp GMIC_PATH=gmic-clone/src || exit 1
 (make && make install) || exit 1
 
 #cp -a PhFGimp/build/file-photoflow PhFGimp/build/phf_gimp ../plugins
