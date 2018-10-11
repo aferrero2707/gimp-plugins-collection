ccf4dce77826385b768b633656abddb810a908a4
aferrero2707 <aferrero1975@gmail.com>
2018-10-11 09:41:50 +0200

    Update build-osx.sh

--- travis/build-osx.sh
+++ travis/build-osx.sh
@@ -4,7 +4,7 @@ source ./environment-osx.sh
 brew update
 mkdir plugins || exit 1
 bash ./build-common-osx.sh || exit 1
-exit
+#exit
 bash ./${TARGET_PLUGIN}/build-osx.sh || exit 1
 
 rm -rf plugins-fixed
