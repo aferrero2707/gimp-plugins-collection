ee3dd73b334fc04d69d7615e7c98abb6398eec63
aferrero2707 <aferrero1975@gmail.com>
2018-10-11 10:31:42 +0200

    Update build-osx.sh

--- GMIC/build-osx.sh
+++ GMIC/build-osx.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
 
+cd build
+if [ ! -e gmic-qt ]; then
+	echo "Running git clone https://github.com/c-koi/gmic-qt.git"
+	git clone https://github.com/c-koi/gmic-qt.git || exit 1
+	echo "... finished"
+fi
+cd gmic-qt || exit 1
+if [ ! -e gmic-clone ]; then
+	echo "Running git clone --depth=1 https://framagit.org/dtschump/gmic.git gmic-clone"
+	git clone --depth=1 https://framagit.org/dtschump/gmic.git gmic-clone || exit 1
+	echo "... finished"
+fi
+
 brew cask uninstall oclint
 brew install fftw curl qt || exit 1
 #brew link qt --force || exit 1
@@ -10,17 +23,11 @@ export LD_LIBRARY_PATH="/usr/local/opt/qt/lib:$LD_LIBRARY_PATH"
 
 ls /usr/local/opt/qt/bin
 
+echo "Generating GMIC headers"
+make -C gmic-clone/src CImg.h gmic_stdlib.h || exit 1
 
-cd build
-if [ ! -e gmic-qt ]; then
-	git clone https://github.com/c-koi/gmic-qt.git || exit 1
-fi
-cd gmic-qt || exit 1
-if [ ! -e gmic-clone ]; then
-	git clone --depth=1 https://framagit.org/dtschump/gmic.git gmic-clone || exit 1
-	make -C gmic-clone/src CImg.h gmic_stdlib.h || exit 1
-fi
 
+echo "Compiling the Qt plug-in"
 qmake QMAKE_CFLAGS+="${CFLAGS} -O2" QMAKE_CXXFLAGS+="${CXXFLAGS} -O2" QMAKE_LFLAGS+="${LDFLAGS} -L/usr/X11/lib -lX11 " CONFIG+=Release HOST=gimp GMIC_PATH=gmic-clone/src || exit 1
 (make && make install) || exit 1
 
