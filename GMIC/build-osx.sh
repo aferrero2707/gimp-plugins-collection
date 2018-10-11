1f61e679987db292ad1d731ffeaee53e45b1c246
aferrero2707 <aferrero1975@gmail.com>
2018-10-11 12:46:06 +0200

    Update build-osx.sh

--- GMIC/build-osx.sh
+++ GMIC/build-osx.sh
@@ -29,6 +29,6 @@ make -C gmic-clone/src CImg.h gmic_stdlib.h || exit 1
 
 echo "Compiling the Qt plug-in"
 qmake QMAKE_CFLAGS+="${CFLAGS} -O2" QMAKE_CXXFLAGS+="${CXXFLAGS} -O2" QMAKE_LFLAGS+="${LDFLAGS} -L/usr/X11/lib -lX11 " CONFIG+=Release HOST=gimp GMIC_PATH=gmic-clone/src || exit 1
-(make && make install) || exit 1
+(make -j 3 && make -j 3 install) || exit 1
 
 cp -a gmic_gimp_qt.app/Contents/MacOS/gmic_gimp_qt ../../plugins
