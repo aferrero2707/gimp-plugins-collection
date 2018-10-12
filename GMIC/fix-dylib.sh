2822675e16b532ad7a48d6efea01508134938f31
aferrero2707 <aferrero1975@gmail.com>
2018-10-12 14:45:30 +0200

    More fixes for Qt frameworks

--- GMIC/fix-dylib.sh
+++ GMIC/fix-dylib.sh
@@ -34,15 +34,38 @@ while [ $I -le $NDY ]; do
 	# check if this is a Qt framework
 	TEST=$(echo "$DYLIB" | grep '\.framework' | grep '/usr/local/opt/qt')
 	if [ -n "$TEST" ]; then
-		DYLIBPATH="$(echo "$DYLIB" | sed "s|/usr/local/opt/qt/lib/||g")"
-		DYLIBPATH="$(dirname "$DYLIBPATH")"
-		mkdir -p "../plugins-fixed/$PLUGIN/Frameworks/$DYLIBPATH" || exit 1
-		echo "cp -a \"$DYLIB\" \"../plugins-fixed/$PLUGIN/Frameworks/$DYLIBPATH\""
-		cp -a "$DYLIB" "../plugins-fixed/$PLUGIN/Frameworks/$DYLIBPATH" || exit 1
-		DYLIB2=$(echo "$DYLIB" | sed "s|/usr/local/opt/qt/lib|@loader_path/$PLUGIN/Frameworks|g")
+		DYLIBNAME="$(basename "$DYLIB")"
+		mkdir -p "../plugins-fixed/$PLUGIN/Frameworks/" || exit 1
+		echo "cp -a \"$DYLIB\" \"../plugins-fixed/$PLUGIN/Frameworks\""
+		cp -a "$DYLIB" "../plugins-fixed/$PLUGIN/Frameworks" || exit 1
+		DYLIB2="@loader_path/$PLUGIN/Frameworks/$DYLIBNAME"
 		echo "DYLIB2: $DYLIB2"
 		echo "install_name_tool -change \"$DYLIB\" \"$DYLIB2\" \"$F2\""
 		install_name_tool -change "$DYLIB" "$DYLIB2" "$F2"
+
+		DYLIST2=$(otool -L "../plugins-fixed/$PLUGIN/Frameworks/$DYLIBNAME")
+		NDY2=$(echo "$DYLIST2" | wc -l)
+		echo "NDY2: $NDY2"
+		I2=2
+		while [ $I2 -le $NDY2 ]; do
+			LINE2=$(echo "$DYLIST2" | sed -n ${I}p)
+			DYLIB2=$(echo $LINE2 | sed -e 's/^[ \t]*//' | tr -s ' ' | tr ' ' '\n' | head -n 1)
+			DYLIB2NAME="$(basename "$DYLIB2")"
+			echo "install_name_tool -change \"$DYLIB2\" \"@loader_path/$DYLIB2NAME\" \"../plugins-fixed/$PLUGIN/Frameworks/$DYLIBNAME\""
+			install_name_tool -change "$DYLIB2" "@loader_path/$DYLIB2NAME" "../plugins-fixed/$PLUGIN/Frameworks/$DYLIBNAME"
+			I2=$((I2+1))
+		done
+		otool -L "../plugins-fixed/$PLUGIN/Frameworks/$DYLIBNAME"
+
+		#DYLIBPATH="$(echo "$DYLIB" | sed "s|/usr/local/opt/qt/lib/||g")"
+		#DYLIBPATH="$(dirname "$DYLIBPATH")"
+		#mkdir -p "../plugins-fixed/$PLUGIN/Frameworks/$DYLIBPATH" || exit 1
+		#echo "cp -a \"$DYLIB\" \"../plugins-fixed/$PLUGIN/Frameworks/$DYLIBPATH\""
+		#cp -a "$DYLIB" "../plugins-fixed/$PLUGIN/Frameworks/$DYLIBPATH" || exit 1
+		#DYLIB2=$(echo "$DYLIB" | sed "s|/usr/local/opt/qt/lib|@loader_path/$PLUGIN/Frameworks|g")
+		#echo "DYLIB2: $DYLIB2"
+		#echo "install_name_tool -change \"$DYLIB\" \"$DYLIB2\" \"$F2\""
+		#install_name_tool -change "$DYLIB" "$DYLIB2" "$F2"
 	fi
 	I=$((I+1))
 done
