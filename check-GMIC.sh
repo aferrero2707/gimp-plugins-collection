266c648f11949b25dc00d08a802a6ae1ece79da4
aferrero2707 <aferrero1975@gmail.com>
2018-11-14 14:52:32 +0100

    Created check branch

---
+++ check-GMIC.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+#rm -rf gmic-qt ;git clone https://github.com/c-koi/gmic-qt.git || exit 1
+cd gmic-qt || exit 1
+git rev-parse --verify HEAD > /tmp/commit-GMIC-new.hash
+#rm -rf gmic-clone; git clone https://github.com/dtschump/gmic.git gmic-clone || exit 1
+cd gmic-clone
+git rev-parse --verify HEAD >> /tmp/commit-GMIC-new.hash
+cd ../../
+
+HASH=$(cat assets.txt | grep "^GMIC-" | grep '.hash$')
+echo "Commit HASH: \"$HASH\""
+
+if [ -n "$HASH" ]; then
+	echo "curl -L https://github.com/${REPO_SLUG}/releases/tag/${RELEASE_TAG}/$HASH"
+	curl -L https://github.com/${REPO_SLUG}/releases/tag/${RELEASE_TAG}/$HASH > /tmp/commit-GMIC-old.hash 
+	result=$?
+	if [ x"$result"	= "x0" ]; then
+		diff -q /tmp/commit-GMIC-new.hash /tmp/commit-GMIC-old.hash
+		result=$?
+		if [ x"$result"	= "x0" ]; then exit; fi
+	fi
+fi
+
+echo "Triggering rebuild of GMIC"
+
+git clone -b master https://github.com/aferrero2707/gimp-plugins-collection.git /tmp/gimp-plugins-collection
+cd /tmp/gimp-plugins-collection
+git checkout -b GMIC
+git merge master
+cat travis.yml.template | sed -e 's|%OS%|osx|g' | sed -e 's|%PLUGIN%|GMIC|g' > .travis.yml
+git add -A
+git commit -m "Updated Travis configuration"
+git push https://aferrero2707:${GITHUB_TOKEN}@github.com/${REPO_SLUG}.git GMIC
---
+++ check-plugins.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+export REPO_SLUG="aferrero2707/gimp-plugins-collection"
+export RELEASE_TAG=continuous
+#ASSETS=$(bash ./get-assets.sh)
+#echo "$ASSETS" > assets.txt
+echo "ASSETS:"
+cat assets.txt
+
+while IFS='' read -r line || [[ -n "$line" ]]; do 
+	bash ./check-${line}.sh
+done < plugin-list.txt
