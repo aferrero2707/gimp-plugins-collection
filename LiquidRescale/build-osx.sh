8f131e9236283564234cdd16b5d53ff8a971be93
aferrero2707 <aferrero1975@gmail.com>
2018-11-15 13:06:55 +0100

    Fixed copy of plugin executables

--- LiquidRescale/build-osx.sh
+++ LiquidRescale/build-osx.sh
@@ -8,4 +8,5 @@ cd liblqr && ./configure --prefix=/usr/local && make -j 2 && make install) || ex
 (git clone https://github.com/carlobaldassi/gimp-lqr-plugin.git && \
 cd gimp-lqr-plugin && ./configure --prefix=/usr/local && make -j 2 && make install) || exit 1
 
-cp -a ${PREFIX}/lib/gimp/2.0/plug-ins/* ../plugins
+cp -a ${PREFIX}/lib/gimp/2.0/plug-ins/gimp-lqr-plugin ../plugins
+cp -a ${PREFIX}/lib/gimp/2.0/plug-ins/plug_in_lqr_iter ../plugins
