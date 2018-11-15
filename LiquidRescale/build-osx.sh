04682741ddc657a366c8107fd8e6beea90544cd0
aferrero2707 <aferrero1975@gmail.com>
2018-11-15 15:51:58 +0100

    Added commit hash uploading for liquid rescale

--- LiquidRescale/build-osx.sh
+++ LiquidRescale/build-osx.sh
@@ -3,10 +3,12 @@
 cd build
 
 (git clone https://github.com/carlobaldassi/liblqr.git && \
-cd liblqr && ./configure --prefix=/usr/local && make -j 2 && make install) || exit 1
+cd liblqr && git rev-parse --verify HEAD > /tmp/commit-LiquidRescale-new.hash && \
+./configure --prefix=/usr/local && make -j 2 && make install) || exit 1
 
 (git clone https://github.com/carlobaldassi/gimp-lqr-plugin.git && \
-cd gimp-lqr-plugin && ./configure --prefix=/usr/local && make -j 2 && make install) || exit 1
+cd gimp-lqr-plugin && git rev-parse --verify HEAD >> /tmp/commit-LiquidRescale-new.hash && \
+./configure --prefix=/usr/local && make -j 2 && make install) || exit 1
 
 cp -a ${PREFIX}/lib/gimp/2.0/plug-ins/gimp-lqr-plugin ../plugins
 cp -a ${PREFIX}/lib/gimp/2.0/plug-ins/plug_in_lqr_iter ../plugins
