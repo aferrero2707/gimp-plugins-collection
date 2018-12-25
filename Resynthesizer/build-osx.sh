90c51a70725da33bae663b947a7b89bfc4de5d7c
aferrero2707 <aferrero1975@gmail.com>
2018-12-25 22:02:52 +0100

    Fixed installed plugins path

--- Resynthesizer/build-osx.sh
+++ Resynthesizer/build-osx.sh
@@ -7,5 +7,7 @@ cd build
 cd resynthesizer && git rev-parse --verify HEAD > /tmp/commit-Resynthesizer-new.hash && \
 ./autogen.sh --prefix=/usr/local && make -j 2 install) || exit 1
 
-cp -a ${PREFIX}/lib/gimp/2.0/plug-ins/gimp-lqr-plugin ../plugins
-cp -a ${PREFIX}/lib/gimp/2.0/plug-ins/plug_in_lqr_iter ../plugins
+cp -a ${PREFIX}/lib/gimp/2.0/plug-ins/resynthesizer* ../plugins
+for P in plugin-heal-selection.py plugin-resynth-sharpen.py plugin-resynth-enlarge.py plugin-uncrop.py plugin-render-texture.py plugin-map-style.py plugin-heal-transparency.py plugin-resynth-fill-pattern.py; do
+	cp -a "/usr/local/lib/gimp/2.0/plug-ins/$P" ../plugins
+done
