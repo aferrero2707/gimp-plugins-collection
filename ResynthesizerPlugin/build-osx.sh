9e43aadba190dba93de61d2479c82abd97c90874
aferrero2707 <aferrero1975@gmail.com>
2018-12-25 22:59:45 +0100

    Renamed Resynthetizer folder

--- ResynthesizerPlugin/build-osx.sh
+++ ResynthesizerPlugin/build-osx.sh
@@ -4,7 +4,7 @@ mkdir -p build || exit 1
 cd build
 
 (git clone https://github.com/bootchk/resynthesizer.git && \
-cd resynthesizer && git rev-parse --verify HEAD > /tmp/commit-Resynthesizer-new.hash && \
+cd resynthesizer && git rev-parse --verify HEAD > /tmp/commit-ResynthesizerPlugin-new.hash && \
 ./autogen.sh --prefix=/usr/local && make -j 2 install) || exit 1
 
 cp -a ${PREFIX}/lib/gimp/2.0/plug-ins/resynthesizer* ../plugins
