0b0905de9e3f27804dd17007720b3c078fb1f03f
aferrero2707 <aferrero1975@gmail.com>
2019-04-02 16:12:34 +0200

    Added linux build scripts

--- check-GMIC.sh
+++ check-GMIC.sh
@@ -12,9 +12,13 @@ cd CImg || exit 1
 git rev-parse --verify HEAD >> /tmp/commit-GMIC-new.hash
 cd ..
 
-HASH=$(cat assets.txt | grep "^GMIC-" | grep '.hash$')
+#for OS in "osx" "linux"; do
+for OS in "linux"; do
+
+HASH=$(cat assets.txt | grep "^GMIC-" | grep "-${OS}" | grep '.hash$')
 echo "Commit HASH: \"$HASH\""
 
+uptodate=0
 if [ -n "$HASH" ]; then
 	echo "curl -L https://github.com/${REPO_SLUG}/releases/download/${RELEASE_TAG}/$HASH"
 	curl -L https://github.com/${REPO_SLUG}/releases/download/${RELEASE_TAG}/$HASH > /tmp/commit-GMIC-old.hash 
@@ -24,19 +28,23 @@ if [ -n "$HASH" ]; then
 		echo "commit-GMIC-new.hash:"; cat /tmp/commit-GMIC-new.hash
 		diff -q /tmp/commit-GMIC-new.hash /tmp/commit-GMIC-old.hash
 		result=$?
-		if [ x"$result"	= "x0" ]; then exit; fi
+		if [ x"$result"	= "x0" ]; then uptodate=1; fi
 	fi
 fi
 
-echo "Triggering rebuild of GMIC"
+if [ x"$uptodate" = "x1" ]; then continue; fi
+
+echo "Triggering rebuild of GMIC under $OS"
 
 git clone -b master https://github.com/aferrero2707/gimp-plugins-collection.git /tmp/gimp-plugins-collection
 cd /tmp/gimp-plugins-collection
-git checkout -b GMIC
-git pull origin GMIC
+git checkout -b GMIC-${OS}
+git pull origin GMIC-${OS}
 git merge master
-cat travis.yml.template | sed -e 's|%OS%|osx|g' | sed -e 's|%PLUGIN%|GMIC|g' > .travis.yml
+cat travis.yml.template | sed -e 's|%OS%|$OS|g' | sed -e 's|%PLUGIN%|GMIC|g' > .travis.yml
 echo "$RANDOM" > random.txt
 git add -A
 git commit -m "Updated Travis configuration"
-git push https://aferrero2707:${GITHUB_TOKEN}@github.com/${REPO_SLUG}.git GMIC
+git push https://aferrero2707:${GITHUB_TOKEN}@github.com/${REPO_SLUG}.git GMIC-${OS}
+
+done
