086c716e4aeeff2636d80daf0f1da6151c8615d0
aferrero2707 <aferrero1975@gmail.com>
2019-04-04 15:33:55 +0200

    Fixed linux build scripts for LiquidRescale

--- LiquidRescale/startup-linux.sh
+++ LiquidRescale/startup-linux.sh
@@ -1,1 +1,1 @@
-export LD_LIBRARY_PATH="$HOME/.config/GIMP-AppImage/2.10/plug-ins/ResynthesizerPlugin/usr/lib":$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="$HOME/.config/GIMP-AppImage/2.10/plug-ins/LiquidRescale/usr/lib":$LD_LIBRARY_PATH
