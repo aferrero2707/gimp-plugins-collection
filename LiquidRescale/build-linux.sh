8b97d3f52597f8b77f2d7e0f03dfffab88dfae79
aferrero2707 <aferrero1975@gmail.com>
2019-06-09 22:27:15 +0200

    Fixed LD_LIBRARY_PATH

--- LiquidRescale/build-linux.sh
+++ LiquidRescale/build-linux.sh
@@ -7,6 +7,7 @@ gimplibdir=$(pkg-config --variable=gimplibdir gimp-2.0)
 echo "gimplibdir: $gimplibdir"
 if [ -z "$gimplibdir" ]; then exit 1; fi
 
+export LD_LIBRARY_PATH="/usr/local/lib/pkgconfig:$LD_LIBRARY_PATH"
 export PKG_CONFIG_PATH="/usr/local/lib/pkgconfig:$PKG_CONFIG_PATH"
 
 
