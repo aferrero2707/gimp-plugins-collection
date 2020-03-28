166772503595720e163699705a55f00bff504eb7
aferrero2707 <aferrero1975@gmail.com>
2020-03-28 17:15:13 +0100

    Update environment-linux.sh

--- environment-linux.sh
+++ environment-linux.sh
@@ -4,7 +4,7 @@ export GIMP_PREFIX="/usr/local/gimp"
 
 export PATH="${GIMP_PREFIX}/bin:$PATH"
 #export LD_LIBRARY_PATH="${GIMP_PREFIX}/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="${GIMP_PREFIX}/lib/pkgconfig:$PKG_CONFIG_PATH"
+export PKG_CONFIG_PATH="${GIMP_PREFIX}/lib/pkgconfig:${GIMP_PREFIX}/lib64/pkgconfig:$PKG_CONFIG_PATH"
 export ACLOCAL_PATH="${GIMP_PREFIX}/share/aclocal:$ACLOCAL_PATH"
 export ACLOCAL_FLAGS="-I ${GIMP_PREFIX}/share/aclocal $ACLOCAL_FLAGS"
 
